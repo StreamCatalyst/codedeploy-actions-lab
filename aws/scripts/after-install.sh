fa4028cc2b13be8d5311fe056cd5876f7b85d171
talent <aws.azue.gcp@gmail.com>
2025-10-01 23:22:20 -0400

    workflow

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 # Download the WAR built by the CI job
 aws s3 cp \
-  s3://codedeploystack-webappdeploymentbucket-hkgdj6ywzzce/SpringBootHelloWorldExampleApplication.war \
+  s3://codedeploystack-webappdeploymentbucket-c95brnwz9iro/SpringBootHelloWorldExampleApplication.war \
   /usr/local/tomcat9/webapps/ROOT.war
 
 # Ensure correct ownership
